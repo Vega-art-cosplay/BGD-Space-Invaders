975f361f31ab2e9c5fe9a310c57b56724dd7a859
Vega-art-cosplay <hasegawa.vega@gmail.com>
2024-12-16 02:24:57 +0200

    Change mid enemy movement

---
+++ Assets/Scripts/Enemy/EnemyControllerMid.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyControllerMid : MonoBehaviour
+{
+    public float minPosX;
+    public float maxPosX;
+
+    public float moveDistance = 1f;
+
+    bool isMovingLeft = true;
+
+
+    public float timeStep = 1f;
+    public float countdown;
+
+    // I added a switch to try both methods
+    public bool isUsingCountdown = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (isUsingCountdown)
+        {
+            countdown = timeStep;
+        }
+        else
+        {
+            // Invoke repeating will be called once after timeStep (2nd parameter) amount,
+            // and then repeatedly every timeStep (3rd parameter) amount
+            //InvokeRepeating("Move", timeStep, timeStep);
+
+            InvokeRepeating("Move", timeStep, timeStep);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isUsingCountdown)
+        {
+            countdown -= Time.deltaTime;
+
+            if (countdown <= 0)
+            {
+                Move();
+                countdown = timeStep;
+            }
+        }
+
+
+
+    }
+
+    void Move()
+    {
+        if (isMovingLeft)
+        {
+            // Moving left
+            Vector3 currentPos = transform.position;
+            Vector3 newPos = currentPos - new Vector3(moveDistance, 0f);
+            transform.position = newPos;
+
+            // If aliens group reached the right-most edge, flip their direction
+            if (transform.position.x <= minPosX)
+            {
+                isMovingLeft = false;
+            }
+        }
+        else
+        {
+            // Moving left
+            Vector3 currentPos = transform.position;
+            Vector3 newPos = currentPos + new Vector3(moveDistance, 0f);
+            transform.position = newPos;
+
+            // If aliens group reached the left-most edge, flip their direction
+            if (transform.position.x >= maxPosX)
+            {
+                isMovingLeft = true;
+            }
+        }
+    }
+}
