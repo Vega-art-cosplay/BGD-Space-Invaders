cc44b9d8a5b292a7a8081b5750a35edb65a2feb5
Vega-art-cosplay <hasegawa.vega@gmail.com>
2024-12-16 18:25:58 +0200

    Add mutton to gameover scene

---
+++ Assets/ButtonControl2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ButtonControl2 : MonoBehaviour
+
+{
+
+    public Button restartButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        restartButton.onClick.AddListener(TaskOnClick);
+    }
+
+
+    void TaskOnClick()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        //print("You have clicked the button!");
+        SceneManager.LoadScene("StartScreen");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
